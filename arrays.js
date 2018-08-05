7691b6890b8745ebede5c2aabfa758c28bbcc901
saraluk <saralukkai@gmail.com>
2018-08-05 03:11:24 +0000

    automatically backed up by learn

--- arrays.js
+++ arrays.js
@@ -4,7 +4,7 @@ function addElementToBeginningOfArray(array=[],element){
   return [element,...array];
 }
 
-function destructivelyAddElementToBeginningOfArray(array=[],secondElement){
+function destructivelyAddElementToBeginningOfArray(array,secondElement){
   return array.unshift(secondElement);
 }
 
